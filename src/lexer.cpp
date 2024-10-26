e85da10153e94a8c047913afe6c8a8024fdd3da9
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2024-10-26 18:42:56 +0900

    Improve lexer by using Location info
    Co-authored-by: Takahiro Numasawa <numa123@users.noreply.github.com>

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -11,69 +11,64 @@ std::ostream &operator<<(std::ostream &os, const Token &token) {
 }
 
 std::string_view Lexer::head() { return input_.substr(pos_); }
+void Lexer::advanceN(size_t n) {
+  pos_ += n;
+  currentLocation_.col += n;
+}
+void Lexer::advance() { advanceN(1); }
+bool Lexer::atEof() const { return pos_ >= input_.size(); }
+void Lexer::nextLine() {
+  assert(input_[pos_] == '\n' &&
+         "nextLine() must be called at the beginning of a line");
+  pos_++;
+  currentLocation_.line++;
+  currentLocation_.col = 0;
+}
 std::vector<Token> Lexer::tokenize() {
   std::vector<Token> tokens;
 
-  while (pos_ < input_.size()) {
+  while (!atEof()) {
     // tokenize integer
     if (std::isdigit(input_[pos_])) {
       const auto start_pos = pos_;
+      auto loc = currentLocation();
       while (std::isdigit(input_[pos_])) {
         if (start_pos == pos_ && input_[pos_] == '0') {
-          pos_++;
+          advance();
           break;
         }
-        pos_++;
+        advance();
       }
       std::string_view num_lit = input_.substr(start_pos, pos_ - start_pos);
-      tokens.emplace_back(Token::TokenKind::NumLit, num_lit);
+      tokens.emplace_back(Token::TokenKind::NumLit, num_lit, loc);
       continue;
     }
 
-    if (input_[pos_] == ' ' || input_[pos_] == '\n') {
-      pos_++;
+    if (input_[pos_] == ' ') {
+      advance();
       continue;
     }
 
-    // tokenize plus
-    if (input_[pos_] == '+') {
-      tokens.emplace_back(Token::TokenKind::Plus, "+");
-      pos_++;
+    if (input_[pos_] == '\n') {
+      nextLine();
       continue;
     }
 
-    // tokenize minus
-    if (input_[pos_] == '-') {
-      tokens.emplace_back(Token::TokenKind::Minus, "-");
-      pos_++;
-      continue;
-    }
+    const auto token_mapping = {
+        std::pair<char, Token::TokenKind>{'+', Token::TokenKind::Plus},
+        {'-', Token::TokenKind::Minus},
+        {'*', Token::TokenKind::Star},
+        {'/', Token::TokenKind::Slash},
+        {'(', Token::TokenKind::OpenParen},
+        {')', Token::TokenKind::CloseParen},
+    };
 
-    // tokenize mul
-    if (input_[pos_] == '*') {
-      tokens.emplace_back(Token::TokenKind::Star, "*");
-      pos_++;
-      continue;
-    }
-
-    // tokenize slash
-    if (input_[pos_] == '/') {
-      tokens.emplace_back(Token::TokenKind::Slash, "/");
-      pos_++;
-      continue;
-    }
-
-    // tokenize (
-    if (input_[pos_] == '(') {
-      tokens.emplace_back(Token::TokenKind::OpenParen, "(");
-      pos_++;
-      continue;
-    }
-    // tokenize (
-    if (input_[pos_] == ')') {
-      tokens.emplace_back(Token::TokenKind::CloseParen, ")");
-      pos_++;
-      continue;
+    for (const auto &[c, kind] : token_mapping) {
+      if (input_[pos_] == c) {
+        tokens.emplace_back(kind, input_.substr(pos_, 1), currentLocation());
+        advance();
+        break;
+      }
     }
   }
 
